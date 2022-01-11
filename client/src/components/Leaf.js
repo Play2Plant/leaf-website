9d2e44bfc0126814b4745edfaf5c2394c6b903be
Boyquotes <webmaster@linux-live-cd.org>
2022-01-11 19:44:44 +0100

    Claim leaf reward

--- client/src/components/Leaf.js
+++ client/src/components/Leaf.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import { Row, Col, Container, Card, Button } from 'react-bootstrap'
+import {
+  checkConnection,
+  getTokenURI,
+  getTokensByAddress,
+  connectWallet,
+switchChain,
+checkNetwork
+} from '../lib/web3'
+import axios from 'axios'
 
 const Leaf = () => {
+
+    const claimLeaf = async () => {
+        await claimLeaf()
+    }
+
+    const pas = 12300;
     return (
         <div>
             <h1>This is Leaf Page</h1>
+	    <p>Vous avez marché {pas}</p>
+
+                  <Button onClick={() => claimLeaf()}>
+                    Récupère tes Leafs
+                  </Button>
+ 	
         </div>
     )
 }
